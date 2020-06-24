20f7fbb473a6c480eb5ce51ed7e6d70c2d7e1145
bmenaman <mail>
2020-06-24 15:02:32 +0100

    handle empty query

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,6 +3,8 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
+        if(query == null)
+            return "";
         if (query.toLowerCase().contains("romeo and juliet")) {
             return "William Shakespeare";
         }
